ee3249d34f708066ba44029a957a2cea69049fdc
farhanyp <36284284+farhanyp@users.noreply.github.com>
2024-01-29 13:32:40 +0700

    add get signature

--- src/api/v1/payment/controller.js
+++ src/api/v1/payment/controller.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { createPayments, getPayments, notificationPayments } from "../../../service/prisma/payment.js";
+import crypto from "crypto-js";
+import { serverKey } from "../../../config.js";
 
 const create = async (req, res, next) => {
   try {
@@ -31,6 +33,23 @@ const indexPayments = async (req, res, next) => {
   }
 };
 
+const getSignature = async (req, res, next) => {
+  try {
+    
+    const {order_id, status_code, gross_amount} = req.body
+
+    const result = crypto.SHA512(order_id + status_code + gross_amount + serverKey).toString()
+
+    res.status(StatusCodes.CREATED).json({ 
+      data: result
+    });
+
+  } catch (error) {
+    next(error);
+
+  }
+};
+
 const notification = async (req, res, next) => {
   try {
     
@@ -49,5 +68,6 @@ const notification = async (req, res, next) => {
 export default {
   create,
   notification,
-  indexPayments
+  indexPayments,
+  getSignature
 };
--- src/api/v1/payment/route.js
+++ src/api/v1/payment/route.js
@@ -7,6 +7,7 @@ const router = new express.Router();
 
 router.post("/api/v1/payment", authenticateUser, authorizeRoles('user'), PaymentController.create);
 router.get("/api/v1/payment", authenticateUser, authorizeRoles('user'), PaymentController.indexPayments);
+router.post("/api/v1/payment/signature", authenticateUser, authorizeRoles('user'), PaymentController.getSignature);
 router.post("/api/v1/payment/notification", authenticateUser, authorizeRoles('user'), PaymentController.notification);
 
 export default router;
